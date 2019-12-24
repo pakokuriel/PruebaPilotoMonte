71385fc1dbf84ce2212af74a037428251eb06c2b
pakokuriel <pakokuriel@gmail.com>
2019-12-24 03:55:53 -0600

    primer branch

---
+++ src/main/java/PruebaPilotoMonte/PruebaPilotoMonte/App.java
@@ -0,0 +1,13 @@
+package PruebaPilotoMonte.PruebaPilotoMonte;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Quarksoft!" );
+    }
+}
