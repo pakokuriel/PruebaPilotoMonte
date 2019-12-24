5f41088d8e845e6753ee95bd43d26eb45b0e42f2
pakokuriel <pakokuriel@gmail.com>
2019-12-24 11:17:30 -0600

    se agregan cambios al branch

--- src/main/java/PruebaPilotoMonte/PruebaPilotoMonte/App.java
+++ src/main/java/PruebaPilotoMonte/PruebaPilotoMonte/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Quarksoft estoy probando!" );
+        System.out.println( "Hola Quarksoft estoy probando!" );
     }
 }
