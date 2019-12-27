0d13eca35d31c4427e117a185d1ecd919950679b
pakokuriel <pakokuriel@gmail.com>
2019-12-27 11:25:50 -0600

    se agregan cambios de prueba3

--- src/test/java/PruebaPilotoMonte/PruebaPilotoMonte/CalcTest.java
+++ src/test/java/PruebaPilotoMonte/PruebaPilotoMonte/CalcTest.java
@@ -4,14 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CalculadoraTest {
+public class CalcTest {
 
 	@Test
 	public void testSuma() {
 		Calculadora calc= new Calculadora();
 		double a = 10;
 		double b= 2;
-		double resultadoEsperado = 12;
+		double resultadoEsperado = 10;
 		
 		assertEquals(resultadoEsperado, calc.suma(a, b), 0);
 		
