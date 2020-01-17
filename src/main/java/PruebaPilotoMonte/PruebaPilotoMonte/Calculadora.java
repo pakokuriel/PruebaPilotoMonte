1508ec69e06f223a78e8ab4d9322326f6c86eced
pakokuriel <pakokuriel@gmail.com>
2020-01-17 12:09:13 -0600

    se inician los cambios de calculadora 3

--- src/main/java/PruebaPilotoMonte/PruebaPilotoMonte/Calculadora.java
+++ src/main/java/PruebaPilotoMonte/PruebaPilotoMonte/Calculadora.java
@@ -14,11 +14,11 @@ public class Calculadora {
 		double res= a-b;
 		return res;
 	}
-/*
+
 	public double multiplica (double a, double b)
 	{
 		return a*b;
-	}*/
+	}
 	
 	public double divide(double a, double b)
 	{
